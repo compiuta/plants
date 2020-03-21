420e343f1cebf36a1758ed25274a78c52901bf3a
compiuta <christianr@rocketmail.com>
2020-03-20 19:04:53 -0700

    add initial test submit form function

--- js/controllers/plants-controller.js
+++ js/controllers/plants-controller.js
@@ -1,10 +1,23 @@
 (function(window) {
     "use strict";
+
+    let plantsControllerHelpers = {
+        
+    }
     
     let plantsController = {
-
+        formSearch: function(e) {
+            e.preventDefault();
+            console.log('searching...');
+        },
+        init: function() {
+            app.plantsModel.init();
+            app.plantsView.init();
+            console.log('Plants Controller Initialised');
+        }
     }
 
     window.app = window.app || {};
     window.app.plantsController = plantsController;
+    app.plantsController.init();
 })(window);
