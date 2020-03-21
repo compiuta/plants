70599e2154cc1564dc56900fd2bf1dbbe16991d1
compiuta <christianr@rocketmail.com>
2020-03-20 19:05:47 -0700

    add get elements and bind listener functions

--- js/views/plants-view.js
+++ js/views/plants-view.js
@@ -1,10 +1,25 @@
 (function(window) {
     "use strict";
 
-    let plantsView = {
-
+    let plantsViewHelpers = {
+        getDomElements: function() {
+            app.plantsView.searchForm = document.querySelector('[data-search="form"]');
+            app.plantsView.searchInput = document.querySelector('[data-search="searchInput"]');
+        },
+        bindEvents: function() {
+            app.plantsView.searchForm.addEventListener('submit', app.plantsController.formSearch);
+        }
     }
 
-    window.app = wiindow.app || {};
+    let plantsView = {
+
+        init: function() {
+            plantsViewHelpers.getDomElements();
+            plantsViewHelpers.bindEvents();
+            console.log('Plants View Initialised');
+        }
+    }
+
+    window.app = window.app || {};
     window.app.plantsView = plantsView;
 })(window);
