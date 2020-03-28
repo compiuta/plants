935b95e7ef1bb379d5ff60f87e6c6e42a4b7bea0
compiuta <christianr@rocketmail.com>
2020-03-27 18:04:18 -0700

    add get data fetch functionality

--- js/models/plants-model.js
+++ js/models/plants-model.js
@@ -6,6 +6,19 @@
     }
 
     let plantsModel = {
+        getData: function(searchValue) {
+            console.log(searchValue);
+            const searchUrl = `https://plantsdb.xyz/search?Common_Name=${searchValue}`;
+            console.log(searchUrl);
+            fetch(searchUrl)
+            .then((response) => response.json())
+            .then((data) => {
+                app.plantsController.populateData(data);
+            })
+            .catch((error) => {
+                console.error('Error:', error);
+            });
+        },
         init: function() {
             console.log('Plants Model Initialised');
         }
