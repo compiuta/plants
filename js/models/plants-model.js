e66fb87d61f10374e3c02ff40d328947e138982f
compiuta <christianr@rocketmail.com>
2020-08-20 23:47:38 -0700

    modify code to work with new api

--- js/models/plants-model.js
+++ js/models/plants-model.js
@@ -1,25 +1,83 @@
 (function (window) {
     "use strict";
 
-    function getData(searchValue) {
+    function formatData(data) {
+        const plantData = data.results;
+        let plantObjectArr = [];
 
-        const searchUrl = `https://plantsdb.xyz/search?limit=30&${searchValue}`;
-        console.log(searchUrl);
+        plantData.forEach(el => {
+            if (el.recordType === 'SPECIES') {
+                const plantObject = {
+                    id: el.elementGlobalId,
+                    Symbol: el.elcode,
+                    Scientific_Name_x: el.scientificName,
+                    Species: el.speciesGlobal.family,
+                    Common_Name: el.primaryCommonName,
+                    Class: el.speciesGlobal.taxclass,
+                    SubKingdom: el.speciesGlobal.phylum,
+                    Kingdom: el.speciesGlobal.kingdom,
+                    Genus: el.speciesGlobal.genus
+                }
+    
+                plantObjectArr.push(plantObject);
+            }
+        });
 
-        fetch(searchUrl)
+        return plantObjectArr;
+
+    }
+
+    function getData(searchValue, populatePage) {
+        const searchUrl = `https://explorer.natureserve.org/api/data/search`;
+    
+        const sendObj = {
+            "criteriaType" : "combined",
+            "textCriteria" : [
+              {
+            "paramType" : "quickSearch",
+            "searchToken" : `${searchValue}`
+          }
+          ],
+            "statusCriteria" : [ ],
+            "locationCriteria" : [ ],
+            "pagingOptions" : {
+              "page" : null,
+              "recordsPerPage" : null
+            },
+            "recordSubtypeCriteria" : [ ],
+            "modifiedSince" : null,
+            "locationOptions" : null,
+            "classificationOptions" : null,
+            "recordTypeCriteria" : [ ]
+          }
+          console.log(searchValue);
+
+        fetch(searchUrl, {
+           method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+              },
+            body: JSON.stringify(sendObj)
+        })
             .then((response) => response.json())
             .then((data) => {
 
-                app.plantsController.searchData = data;
+                const plantData = formatData(data);
 
-                if (searchValue.indexOf('id') > -1) {
-                    console.log('hello ' + data);
-                    app.plantsController.fetchItemPageData(data.data[0]);
+                app.plantsController.searchData = plantData;
+
+                console.log(plantData)
+
+                if (populatePage) {
+                    console.log('hello ' + plantData);
+                    app.plantsController.fetchItemPageData(plantData);
                 } else {
                     app.plantsController.populateData();
                 }
             })
             .catch((error) => {
+                app.plantsController.searchData = [];
+                app.plantsController.populateData();
                 console.error('Error:', error);
             });
     }
@@ -27,8 +85,8 @@
     console.log('Model Initialised');
 
     const plantsModel = {
-        getData: function (searchValue) {
-            getData(searchValue);
+        getData: function (searchValue, populatePage) {
+            getData(searchValue, populatePage);
         }
     }
 
